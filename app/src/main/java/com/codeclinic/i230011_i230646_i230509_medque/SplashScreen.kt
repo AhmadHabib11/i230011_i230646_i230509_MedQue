8703900dfd7a594b4d1a77d7ab5ade211a3801a1
Ahmad Habib <ahmadd.habibb11@gmail.com>
2025-10-31 01:04:41 +0500

    Splash screen done

--- app/src/main/java/com/codeclinic/i230011_i230646_i230509_medque/SplashScreen.kt
+++ app/src/main/java/com/codeclinic/i230011_i230646_i230509_medque/SplashScreen.kt
@@ -6,15 +6,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.splashscreen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    val intent = Intent(this, Onboarding::class.java)
+                    startActivity(intent)
+                    finish()
+                }, 5000)
+
     }
 }
