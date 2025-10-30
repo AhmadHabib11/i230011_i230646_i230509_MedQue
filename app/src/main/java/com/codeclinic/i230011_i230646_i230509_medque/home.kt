8508feb1cee7e8a5f09bf7841990c48b43c97096
abdullah-x64-cmyk <abdullahajmal646464@gmail.com>
2025-10-30 21:32:29 +0500

    make the homepage screen.

---
+++ app/src/main/java/com/codeclinic/i230011_i230646_i230509_medque/home.kt
@@ -0,0 +1,20 @@
+package com.codeclinic.i230011_i230646_i230509_medque
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
