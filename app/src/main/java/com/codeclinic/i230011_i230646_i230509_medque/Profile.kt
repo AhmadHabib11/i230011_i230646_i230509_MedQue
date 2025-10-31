bd25ad874bd58a837a9650f531bb646fd9901456
AbdullahRasheed452 <abdullah.rasheed452@gmail.com>
2025-10-31 17:11:06 +0500

    changed name of editprofile screen to profile screen. Apologies for confusion earlier

--- app/src/main/java/com/codeclinic/i230011_i230646_i230509_medque/Profile.kt
+++ app/src/main/java/com/codeclinic/i230011_i230646_i230509_medque/Profile.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class EditProfile : AppCompatActivity() {
+class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.editprofile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.edit_profile)) { v, insets ->
+        setContentView(R.layout.profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.profile_screen)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
