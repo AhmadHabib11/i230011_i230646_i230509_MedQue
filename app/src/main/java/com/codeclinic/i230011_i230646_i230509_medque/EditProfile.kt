cf61dcb8acaadb56d61ffb4a2920b283b0467d82
AbdullahRasheed452 <abdullah.rasheed452@gmail.com>
2025-10-31 16:49:05 +0500

    made edit profile screen and did some tweaks in color codes

---
+++ app/src/main/java/com/codeclinic/i230011_i230646_i230509_medque/EditProfile.kt
@@ -0,0 +1,20 @@
+package com.codeclinic.i230011_i230646_i230509_medque
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EditProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.editprofile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.edit_profile)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
